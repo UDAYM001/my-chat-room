c9145fb02e21ebd79e89d96f680d27a50ddcf94c
Adam La Morre <alamorre@gmail.com>
2022-08-05 13:41:32 -0700

    </ChatWrapper>

--- frontend-react/src/chatsPage/index.js
+++ frontend-react/src/chatsPage/index.js
@@ -2,18 +2,22 @@ import { useContext } from "react";
 
 import { Context } from "../functions/context";
 
+import ChatWrapper from "./ChatWrapper";
+
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = () => {
   const { user } = useContext(Context);
 
   return (
-    <PrettyChatWindow
-      projectId="5d498a31-cd23-42b7-b367-4fcc9463bd2f"
-      username={user.username} // "admin"
-      secret={user.secret} // "pbkdf2_sha256$320000$nzEFUmjPOroNRTbdHHoFlp$82AkZZd1ZepvhMSIf4UOqoKv+mrYXtL+9PFfEXxLOhw="
-      style={{ height: "100%" }}
-    />
+    <ChatWrapper>
+      <PrettyChatWindow
+        projectId="5d498a31-cd23-42b7-b367-4fcc9463bd2f"
+        username={user.username} // "admin"
+        secret={user.secret} // "pbkdf2_sha256$320000$nzEFUmjPOroNRTbdHHoFlp$82AkZZd1ZepvhMSIf4UOqoKv+mrYXtL+9PFfEXxLOhw="
+        style={{ height: "100%" }}
+      />
+    </ChatWrapper>
   );
 };
 
